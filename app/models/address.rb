aa013c157c4b644cfcf1f74eba5bb3daed74f8a0
github username <github email address>
2021-04-02 18:22:25 -0400

    commit tree 93240a1c976848f19005642b354e75264f64f01a

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,3 @@
 class Address < ActiveRecord::Base
-  belongs_to :person
+  belongs_to :person, :class_name => "People"
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ActiveRecord::Base
-  has_many :addresses
-
+  has_many :addresses, :foreign_key => 'person_id'
+  accepts_nested_attributes_for :addresses
 end
